a07aa6838bc45ac199a2318c26c41b8c33ac419f
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-15 18:19:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,4 +15,10 @@ def valid_move(board, index)
     end
   end
   
-  def 
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+end
