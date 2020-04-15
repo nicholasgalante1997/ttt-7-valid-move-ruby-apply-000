ed689477c1d9407f6388f4b53c2e49d367414c61
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-15 18:14:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
-# code your #valid_move? method here
+def valid_move(board, index)
+  def position_taken? (array, index)
+    if array[index] == "" || array[index] == " " || array[index] == nil
+    return false
+  else
+    return true
+  end
+  end
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def on_board?(Integer)
+  if Integer.between( )
